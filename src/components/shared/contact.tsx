ad20754ee0f75798a46db6c9daa65d37b4efd91f
Sebastien Crepin <seb_crep@hotmail.com>
2020-04-13 17:22:16 +0200

    feat: improve contact section

--- src/components/shared/contact.tsx
+++ src/components/shared/contact.tsx
@@ -1,37 +1,94 @@
 import React from 'react';
 import { css } from '@emotion/core';
 import { useTheme } from 'emotion-theming';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 
+import Button from './button';
 import mq from '../../styles/mq';
 
+import Container from '../styled/container';
+
+import { ImageType } from '../../types/image';
 import { ThemeType } from '../../styles/theme';
 
+const query = graphql`
+  {
+    contact: file(name: { eq: "contact" }) {
+      childImageSharp {
+        fluid(
+          maxWidth: 450
+          maxHeight: 550
+          cropFocus: CENTER
+          fit: COVER
+          quality: 80
+        ) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
+
+interface StaticQueryType {
+  contact: ImageType;
+}
+
 const Contact: React.FC = () => {
+  const { contact } = useStaticQuery<StaticQueryType>(query);
   const { color } = useTheme<ThemeType>();
 
   return (
     <section
       css={css`
         background-color: ${color?.primary};
-        display: flex;
-        align-items: center;
-
-        ${mq(`md`)} {
-          min-height: 700px;
-        }
       `}
     >
-      <div>
-        <h2
+      <Container>
+        <div
           css={css`
-            color: ${color?.white};
+            display: flex;
+            align-items: center;
+            position: relative;
+
+            ${mq(`md`)} {
+              height: 700px;
+            }
           `}
         >
-          A lot of companies are working with us.
-          <br />
-          Are you ready to become our partner?
-        </h2>
-      </div>
+          <div
+            css={css`
+              position: absolute;
+              width: 450px;
+              height: 550px;
+              top: 50%;
+              right: 100px;
+              transform: translateY(-50%);
+              z-index: 1;
+            `}
+          >
+            <Img fluid={contact?.childImageSharp?.fluid} />
+          </div>
+          <div
+            css={css`
+              position: relative;
+              z-index: 2;
+            `}
+          >
+            <h2
+              css={css`
+                color: ${color?.white};
+                margin-bottom: 75px;
+              `}
+            >
+              A lot of companies are working with us.
+              <br />
+              Are you ready to become our partner?
+            </h2>
+            <Button to="/contact">Contact us</Button>
+          </div>
+        </div>
+      </Container>
     </section>
   );
 };
