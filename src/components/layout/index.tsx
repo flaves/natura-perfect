2855b0e7927872a5893b39175e2ba40b1530caa8
Sebastien Crepin <seb_crep@hotmail.com>
2020-04-13 17:22:38 +0200

    feat: add header + footer to layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import Header from './header';
+import Footer from './footer';
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <main role="main">{children}</main>
+  <section>
+    <Header />
+    <main role="main">{children}</main>
+    <Footer />
+  </section>
 );
 
 export default Layout;
