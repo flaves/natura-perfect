fad620cda17f91eea900145b04325563a0f015a8
Sebastien Crepin <seb_crep@hotmail.com>
2020-04-13 15:47:39 +0200

    feat: homepage services

---
+++ src/components/pages/home/services.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Services: React.FC = () => {
+  return (
+    <section>
+      <div>
+        <h2>Services</h2>
+        <p>
+          Nullam sodales rhoncus dignissim. Nulla ut congue quam, vel dapibus
+          nulla. Duis quis neque auctor, congue nunc sed, porttitor ante.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default React.memo(Services);
