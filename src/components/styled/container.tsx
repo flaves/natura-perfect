bcca0ce0916623248043378f5676efc8c5386630
Sebastien Crepin <seb_crep@hotmail.com>
2020-04-13 17:22:22 +0200

    feat: add container

---
+++ src/components/styled/container.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import mq from '../../styles/mq';
+
+const Container = styled.div`
+  padding: 0 20px;
+
+  ${mq(`md`)} {
+    padding: 0 100px;
+  }
+`;
+
+export default Container;
