4ab79f2f7595b30ae95a00b4dfe3b01c5936f6e0
Sebastien Crepin <seb_crep@hotmail.com>
2020-04-26 12:05:46 +0200

    feat: improve link

--- src/components/shared/link.tsx
+++ src/components/shared/link.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { css } from '@emotion/core';
 import { Link as GatsbyLink, GatsbyLinkProps } from 'gatsby';
+import { useTheme } from 'emotion-theming';
+
+import { ThemeType } from '../../styles/theme';
 
 interface LinkProps extends GatsbyLinkProps<any> {
   children: React.ReactNode;
@@ -8,19 +11,49 @@ interface LinkProps extends GatsbyLinkProps<any> {
 }
 
 const Link: React.FC<LinkProps> = ({ children, variant, ...props }) => {
+  const { color } = useTheme<ThemeType>();
+
   const renderVariant = () => {
     switch (variant) {
       case `black`:
         return css`
-          color: black;
+          color: ${color.primary};
+
+          span:first-of-type {
+            background-color: hsl(164, 19%, 77%);
+          }
+
+          &:hover {
+            color: white;
+
+            span:first-of-type {
+              background-color: ${color.primary};
+            }
+          }
         `;
       case `white`:
         return css`
           color: white;
+
+          &:hover {
+            color: hsl(164, 19%, 15%);
+
+            span:first-of-type {
+              background-color: white;
+            }
+          }
         `;
       default:
         return css`
           color: white;
+
+          &:hover {
+            color: hsl(164, 19%, 15%);
+
+            span:first-of-type {
+              background-color: white;
+            }
+          }
         `;
     }
   };
@@ -30,18 +63,19 @@ const Link: React.FC<LinkProps> = ({ children, variant, ...props }) => {
       {...props}
       css={css`
         ${renderVariant()};
+        appearance: none;
+        border: 0;
+        background-color: transparent;
         font-family: Mixta, sans-serif;
         font-size: 30px;
         font-weight: 700;
         position: relative;
         transition: color 0.5s;
         padding: 0 5px;
+        cursor: pointer;
 
         &:hover {
-          color: hsl(164, 19%, 15%);
-
           span:first-of-type {
-            background-color: white;
             height: 38px;
           }
         }
