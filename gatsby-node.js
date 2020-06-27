6def3256ee05bae358807fef529147000c478cda
Sebastien <sebastien@flav.es>
2020-06-27 18:06:24 +0200

    feat: remove process env

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,27 +1,25 @@
 exports.createPages = ({ actions: { createRedirect } }) => {
-  if (process.env.NODE_ENV !== `development`) {
-    createRedirect({
-      fromPath: '/',
-      toPath: '/waiting',
-      isPermanent: true,
-    });
+  createRedirect({
+    fromPath: '/',
+    toPath: '/waiting',
+    isPermanent: true,
+  });
 
-    createRedirect({
-      fromPath: '/about',
-      toPath: '/waiting',
-      isPermanent: true,
-    });
+  createRedirect({
+    fromPath: '/about',
+    toPath: '/waiting',
+    isPermanent: true,
+  });
 
-    createRedirect({
-      fromPath: '/contact',
-      toPath: '/waiting',
-      isPermanent: true,
-    });
+  createRedirect({
+    fromPath: '/contact',
+    toPath: '/waiting',
+    isPermanent: true,
+  });
 
-    createRedirect({
-      fromPath: '/services',
-      toPath: '/waiting',
-      isPermanent: true,
-    });
-  }
+  createRedirect({
+    fromPath: '/services',
+    toPath: '/waiting',
+    isPermanent: true,
+  });
 };
