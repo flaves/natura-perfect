fd7f63dad7c9acc9d417729864ed82c4cc716c18
Sebastien Crepin <sebastien@flav.es>
2022-07-06 22:29:44 +0200

    fix: remove redirection

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,33 +1,33 @@
 exports.createPages = ({ actions: { createRedirect } }) => {
-  createRedirect({
-    fromPath: '/',
-    toPath: '/waiting',
-    isPermanent: true,
-    force: true,
-    redirectInBrowser: true,
-  });
-
-  createRedirect({
-    fromPath: '/about',
-    toPath: '/waiting',
-    isPermanent: true,
-    force: true,
-    redirectInBrowser: true,
-  });
-
-  createRedirect({
-    fromPath: '/contact',
-    toPath: '/waiting',
-    isPermanent: true,
-    force: true,
-    redirectInBrowser: true,
-  });
-
-  createRedirect({
-    fromPath: '/services',
-    toPath: '/waiting',
-    isPermanent: true,
-    force: true,
-    redirectInBrowser: true,
-  });
+  // createRedirect({
+  //   fromPath: '/',
+  //   toPath: '/waiting',
+  //   isPermanent: true,
+  //   force: true,
+  //   redirectInBrowser: true,
+  // });
+  //
+  // createRedirect({
+  //   fromPath: '/about',
+  //   toPath: '/waiting',
+  //   isPermanent: true,
+  //   force: true,
+  //   redirectInBrowser: true,
+  // });
+  //
+  // createRedirect({
+  //   fromPath: '/contact',
+  //   toPath: '/waiting',
+  //   isPermanent: true,
+  //   force: true,
+  //   redirectInBrowser: true,
+  // });
+  //
+  // createRedirect({
+  //   fromPath: '/services',
+  //   toPath: '/waiting',
+  //   isPermanent: true,
+  //   force: true,
+  //   redirectInBrowser: true,
+  // });
 };
